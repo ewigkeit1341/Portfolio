ab98a6f0e3b73a4d7968bb1847d1b3766824a631
ewigkeit1341 <166927562+ewigkeit1341@users.noreply.github.com>
2025-08-08 14:30:28 +0200

    Change img

--- script.js
+++ script.js
@@ -343,7 +343,7 @@ class App extends React.Component {
                                 </div>
                             </div>
                             <div className="hero-image">
-                                <img src="./photo.JPG" alt="Marharyta Lykhvar" className="profile-img" />
+                                <img src="photo.JPG" alt="Marharyta Lykhvar" className="profile-img" />
                             </div>
                         </div>
                     </div>
@@ -529,4 +529,5 @@ class App extends React.Component {
 // Рендерим приложение
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 
