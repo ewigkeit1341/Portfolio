397d372196e09763dc575e2e8f4d2ce2830d670d
ewigkeit1341 <166927562+ewigkeit1341@users.noreply.github.com>
2025-08-10 12:40:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -464,7 +464,7 @@ class App extends React.Component {
                                 </div>
                             </div>
                             <div className="hero-image">
-                                <img src="./photo.jpg" alt="Marharyta Lykhvar" className="profile-img" />
+                                <img src="./photo.jpg" alt="Margarita Lykhvar" className="profile-img" />
                             </div>
                         </div>
                     </div>
@@ -634,6 +634,7 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 
 
 
